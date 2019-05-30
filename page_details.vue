4f2b1f56abd0aadc61244378eb8d688f01c11d92
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-05-30 11:23:06 -0400

    Norco Village 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -3,7 +3,7 @@
         <loading-spinner v-if="!dataLoaded"></loading-spinner>
         <transition name="fade">
             <div v-if="dataLoaded" v-cloak>
-                <div class="inside_header_background" :style="{ background: 'linear-gradient(0deg, rgba(219,108,34, 0.8), rgba(219,108,34, 0.8)), url(' + pageBanner.image_url + ') center center no-repeat rgba(219,108,34)' }">
+                <div class="inside_header_background" :style="{ background: 'linear-gradient(0deg, rgba(141,69,22, 0.8), rgba(141,69,22, 0.8)), url(' + pageBanner.image_url + ') center center no-repeat rgba(141,69,22)' }">
                     <div class="main_container">
                         <h2 v-if="currentPage.title" v-html="currentPage.title"></h2>
                     </div>
