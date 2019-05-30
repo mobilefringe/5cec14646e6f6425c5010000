914b4533808d31deba1719b85cee66f12f78b968
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-05-30 10:45:23 -0400

    Norco Village 1.0  /inside_banner.vue

--- inside_banner.vue
+++ inside_banner.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="inside_header_background" v-if="pageBanner" :style="{ background: 'linear-gradient(0deg, rgba(219,108,34, 0.8), rgba(244,157,102, 0.8)), url(' + pageBanner.image_url + ') center center no-repeat rgba(219,108,34)' }">
+    <div class="inside_header_background" v-if="pageBanner" :style="{ background: 'linear-gradient(0deg, rgba(219,108,34, 0.8), rgba(219,108,34, 0.8)), url(' + pageBanner.image_url + ') center center no-repeat rgba(219,108,34)' }">
         <div class="main_container">
             <div class="page_container">
                 <h2 v-html="pageName"></h2>
