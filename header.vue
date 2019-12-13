bbf633755cc6d91a345c4d05c8525fc5ae8c9225
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-12-13 13:39:26 -0500

    Norco Village 1.1  /header.vue

--- header.vue
+++ header.vue
@@ -39,7 +39,7 @@
                             </span>
                         </div>
                     </div>
-                    <nav id="primary_nav">
+                    <nav id="primary_nav" role="navigation" aria-label="Main">
 						<ul>
 						    <li class="menu_item" v-if="isTablet" v-for="item in menu_items" :id="item.id">
 						        <router-link v-if="item.sub_menu == undefined" :to="item.href">{{ item.name }}</router-link>
@@ -63,7 +63,7 @@
 					</nav>
 					<div class="nav_container visible_phone">
 					    <transition name="custom-classes-transition" enter-active-class="animated slideInRight" leave-active-class="animated slideOutRight">
-    					    <nav id="mobile_nav" v-show="showMenu" class="">
+    					    <nav id="mobile_nav" v-show="showMenu" class="" role="navigation" aria-label="Mobile">
     					        <ul>
     					            <li v-for="(item,key) in menu_items" class="menu_item">
     							        <router-link :to="item.href" v-if="item.sub_menu == undefined">{{$t(item.name)}}</router-link>
